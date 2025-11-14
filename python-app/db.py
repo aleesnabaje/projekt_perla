2882a6ae733698fcead5370018f9bffdedf9bb9b
Zul8k <juliankosobudzki0@gmail.com>
2025-11-14 09:34:41 +0100

    ednpointy: organizator, adres

--- python-app/db.py
+++ python-app/db.py
@@ -45,3 +45,79 @@ class db:
         cursor.close()
         conn.close()
         return { "status":"ok","deleted_id": id } 
+    
+#-----------------------------------------------------------------------------------------------------------------
+    
+    def get_organizator(self):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM organizatorzy")
+        organizator = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return organizator
+    
+    def save_organizator(self, organizator_data):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("INSERT INTO organizatorzy (nazwa) VALUES (%s)", (organizator_data['nazwa'],))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return { "status":"ok","added_id": cursor.lastrowid } 
+    
+    def put_organizator(self, id, organizator_edytuj):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("UPDATE organizatorzy SET nazwa = %s WHERE id = %s", (organizator_edytuj["nazwa"], id))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return { "status":"ok","updated_id": id } 
+    
+    def soft_delete_organizator(self, id):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("UPDATE organizatorzy SET deleted = 1 WHERE id = %s", (id,))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return { "status":"ok","deleted_id": id } 
+    
+#---------------------------------------------------------------------------------------------------------
+    
+    def get_adres(self):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM adresy")
+        adres = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return adres
+    
+    def save_adres(self, adres_data):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("INSERT INTO adresy (adres, wspolrzedne) VALUES (%s, %s)", (adres_data['adres'], adres_data['wspolrzedne']))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return { "status":"ok","added_id": cursor.lastrowid } 
+    
+    def put_adres(self, id, adres_edytuj):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("UPDATE adresy SET adres = %s, wspolrzedne = %s WHERE id = %s", (adres_edytuj["adres"],adres_edytuj['wspolrzedne'], id))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return { "status":"ok","updated_id": id } 
+    
+    def soft_delete_adres(self, id):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("UPDATE adresy SET deleted = 1 WHERE id = %s", (id,))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return { "status":"ok","deleted_id": id } 
--- python-app/main.py
+++ python-app/main.py
@@ -4,7 +4,7 @@ from db import db
 
 app = FastAPI(debug=True)
 
-@app.get("/get-wydarzenie")
+@app.get("/wydarzenie")
 def get_wydarzenie():
     my_db = db()
     return my_db.get_wydarzenie()
@@ -28,4 +28,60 @@ async def soft_delete_wydarzenie(id, request: Request):
     id = id
     my_db = db()
     result = my_db.soft_delete_wydarzenie(id)
+    return JSONResponse(status_code=201, content=result)
+
+#--------------------------------------------------------------------------------
+
+@app.get("/organizator")
+def get_organizator():
+    my_db = db()
+    return my_db.get_organizator()
+
+@app.post("/organizator")
+async def save_organizator(request: Request):
+    organizator_data = await request.json()
+    my_db = db()
+    result = my_db.save_organizator(organizator_data)
+    return JSONResponse(status_code=201, content=result)
+
+@app.put("/organizator/{id}")
+async def put_organizator(id, request: Request):
+    organizator_edytuj = await request.json()
+    my_db = db()
+    result = my_db.put_organizator(id, organizator_edytuj)
+    return JSONResponse(status_code=201, content=result)
+
+@app.delete("/organizator/{id}")
+async def soft_delete_organizator(id, request: Request):
+    id = id
+    my_db = db()
+    result = my_db.soft_delete_organizator(id)
+    return JSONResponse(status_code=201, content=result)
+
+#--------------------------------------------------------------------------------
+
+@app.get("/adres")
+def get_adres():
+    my_db = db()
+    return my_db.get_adres()
+
+@app.post("/adres")
+async def save_adres(request: Request):
+    adres_data = await request.json()
+    my_db = db()
+    result = my_db.save_adres(adres_data)
+    return JSONResponse(status_code=201, content=result)
+
+@app.put("/adres/{id}")
+async def put_adres(id, request: Request):
+    adres_edytuj = await request.json()
+    my_db = db()
+    result = my_db.put_adres(id, adres_edytuj)
+    return JSONResponse(status_code=201, content=result)
+
+@app.delete("/adres/{id}")
+async def soft_delete_adres(id, request: Request):
+    id = id
+    my_db = db()
+    result = my_db.soft_delete_adres(id)
     return JSONResponse(status_code=201, content=result)
